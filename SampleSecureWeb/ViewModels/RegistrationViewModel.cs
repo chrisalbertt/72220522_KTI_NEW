869feaec9f39a136f46415cc21d8ae0431156b36
Albertt <chrisalberttt5@gmail.com>
2024-10-09 23:40:30 +0700

    Implementasi Registrasi dan Login

--- SampleSecureWeb/ViewModels/RegistrationViewModel.cs
+++ SampleSecureWeb/ViewModels/RegistrationViewModel.cs
@@ -10,9 +10,6 @@ public class RegistrationViewModel
 
     [Required]
     [DataType(DataType.Password)]
-    [MinLength(12, ErrorMessage = "Password must be at least 12 characters long.")]
-    [RegularExpression(@"^(?=.[a-z])(?=.[A-Z])(?=.*\d).+$", 
-        ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, and one digit.")]
     public string? Password { get; set; }
 
     [Required]
--- SampleSecureWeb/obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
+++ SampleSecureWeb/obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4738e95cf91193ca987ef8e748fb9e501500bff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d805c036b44a311d54ea7f633c145724e4e34866")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
